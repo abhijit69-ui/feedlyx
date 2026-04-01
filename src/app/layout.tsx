48f4da361dfe3e65efcae7ea3fe10588741146b2
Abhijit Nath <abhijitnath1a1@gmail.com>
2026-04-01 17:28:42 +0530

    feat: add theme support and navigation components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import { ClerkProvider } from '@clerk/nextjs';
+import { ThemeProvider } from '@/components/theme-provider';
+import Navbar from '@/components/Navbar';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -24,14 +26,33 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
-      <html lang='en'>
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
-          {children}
-        </body>
-      </html>
-    </ClerkProvider>
+    <html lang='en' suppressHydrationWarning>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <ClerkProvider>
+          <ThemeProvider
+            attribute='class'
+            defaultTheme='system'
+            enableSystem
+            disableTransitionOnChange
+          >
+            <div className='min-h-screen'>
+              <Navbar />
+              <main className='py-8'>
+                {/* Container to center */}
+                <div className='max-w-7xl mx-auto px-4'>
+                  <div className='grid grid-cols-1 lg:grid-cols-12 gap-6'>
+                    <div className='hidden lg:block lg:col-span-3'>sidebar</div>
+
+                    <div className='lg:col-span-9'>{children}</div>
+                  </div>
+                </div>
+              </main>
+            </div>
+          </ThemeProvider>
+        </ClerkProvider>
+      </body>
+    </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,7 @@
-import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs';
-
 export default function Home() {
   return (
-    <div className='font-sans grid items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20'>
-      <Show when='signed-out'>
-        <SignInButton mode='modal' />
-        <SignUpButton>
-          <button className='bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer'>
-            Sign Up
-          </button>
-        </SignUpButton>
-      </Show>
-      <Show when='signed-in'>
-        <UserButton />
-      </Show>
+    <div className=''>
+      <h1>Home</h1>
     </div>
   );
 }
