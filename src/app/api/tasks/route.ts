552ada12ce746433d42d429539af1c4bc4b7d159
Abhijit Nath <abhijitnath1a1@gmail.com>
2026-04-04 19:01:32 +0530

    feat: implement notifications feature with fetching and marking as read functionality, add skeleton loading UI

---
+++ src/app/api/tasks/route.ts
@@ -0,0 +1,10 @@
+interface Task {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+let task: Task[] = [
+  { id: 1, title: 'Learn Next Js', completed: false },
+  { id: 2, title: 'Build project', completed: false },
+];
